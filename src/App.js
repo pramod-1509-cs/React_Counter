a445a42b6f7e6c2d0c4da4d57917c57eed1c4cef
pramod-1509-cs <104983369+pramod-1509-cs@users.noreply.github.com>
2023-10-28 11:45:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [charLimit, setCharLimit] = useState(50);
+  const [fontSize, setFontSize] = useState(16);
+
+  const handleTextChange = (e) => {
+    const inputValue = e.target.value;
+
+    if (inputValue.length <= charLimit) {
+      setText(inputValue);
+    } else {
+      e.preventDefault();
+    }
+
+    const wordCount = inputValue.split(/\s+/).filter(Boolean).length;
+    const charCount = inputValue.length;
+
+    document.getElementById(
+      "word-counter"
+    ).textContent = `Word Count: ${wordCount}`;
+    document.getElementById(
+      "char-counter"
+    ).textContent = `Character Count: ${charCount}`;
+  };
+
+  const handleCharLimitChange = (e) => {
+    const newCharLimit = parseInt(e.target.value);
+    setCharLimit(newCharLimit);
+  };
+
+  const handleFontSizeChange = (e) => {
+    const newFontSize = e.target.value;
+    setFontSize(newFontSize);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        onChange={handleTextChange}
+        value={text}
+        style={{ fontSize: `${fontSize}px` }}
+      ></textarea>
+      <div id="word-counter">Word Count: 0</div>
+      <div id="char-counter">Character Count: 0</div>
+      <label htmlFor="char-limit-input">Character Limit:</label>
+      <input
+        type="number"
+        id="char-limit-input"
+        value={charLimit}
+        onChange={handleCharLimitChange}
+      />
+      <label htmlFor="fontSize-input">Font Size:</label>
+      <input
+        type="range"
+        id="fontSize-input"
+        min="16"
+        max="32"
+        step="1"
+        value={fontSize}
+        onChange={handleFontSizeChange}
+      />
     </div>
   );
 }
